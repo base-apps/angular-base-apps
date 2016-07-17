553b26302c1ea6d348330739fb54e8c9df15d80f
Brian Soumakian <bsoumakian@gmail.com>
2016-07-17 14:54:17 -0700

    clean up build logic
    use same gulp tasks to build for development and production

--- gulpfile.js
+++ gulpfile.js
@@ -112,13 +112,17 @@ gulp.task('clean:dist', function(cb) {
 
 // Copy static files (but not the Angular templates, Sass, or JS)
 gulp.task('copy', function() {
-  gulp.src(paths.html.base, {
+  var merged = merge();
+
+  merged.add(gulp.src(paths.html.base, {
     base: './docs/'
   })
-    .pipe(gulp.dest('build'));
+    .pipe(gulp.dest('build')));
 
-  return gulp.src('./iconic/**/*')
-    .pipe(gulp.dest('build/assets/img/iconic/'));
+  merged.add(gulp.src('./iconic/**/*')
+    .pipe(gulp.dest('build/assets/img/iconic/')));
+
+  return merged;
 });
 
 // Copy page templates and generate routes
@@ -133,21 +137,41 @@ gulp.task('copy:templates', ['javascript'], function() {
 });
 
 // Copy Foundation directive partials
-gulp.task('copy:partials', ['clean:partials'], function(cb) {
-  gulp.src(paths.html.partials)
+gulp.task('copy:partials', ['clean:partials'], function() {
+  var merged = merge();
+
+  // legacy package
+  merged.add(gulp.src(paths.html.partials)
+  .pipe($.ngHtml2js({
+    prefix: 'components/',
+    moduleName: 'foundation',
+    declareModule: false
+  }))
+  .pipe($.concat('foundation-apps-templates.js'))
+  .pipe(gulp.dest('./build/assets/js'))
+  .pipe($.if(production, $.uglify()))
+  .pipe($.rename('foundation-apps-templates.min.js'))
+  .pipe(gulp.dest('./build/assets/js')));
+
+  // base package
+  merged.add(gulp.src(paths.html.partials)
     .pipe($.ngHtml2js({
       prefix: 'components/',
       moduleName: 'base',
       declareModule: false
     }))
+    .pipe($.concat('base-apps-templates.js'))
+    .pipe(gulp.dest('./build/assets/js'))
     .pipe($.if(production, $.uglify()))
-    .pipe($.concat('templates.js'))
-    .pipe(gulp.dest('./build/assets/js'));
+    .pipe($.rename('base-apps-templates.min.js'))
+    .pipe(gulp.dest('./build/assets/js'))
+    .pipe($.concat('templates.js')) // use same template for docs
+    .pipe(gulp.dest('./build/assets/js')));
 
-  gulp.src('./docs/partials/**/*.html')
-    .pipe(gulp.dest('./build/partials/'));
+  merged.add(gulp.src('./docs/partials/**/*.html')
+    .pipe(gulp.dest('./build/partials/')));
 
-  cb();
+  return merged;
 });
 
 // 5. STYLESHEETS
@@ -167,7 +191,39 @@ gulp.task('css', ['sass'], function() {
 
 // Compile stylesheets
 gulp.task('sass', function() {
-  return gulp.src('docs/assets/scss/app.scss')
+  var merged = merge();
+
+  // legacy package
+  merged.add(gulp.src('scss/foundation.scss')
+    .pipe($.sass({
+      outputStyle: 'nested',
+      errLogToConsole: true
+    }))
+    .pipe($.autoprefixer({
+      browsers: ['> 1%', 'last 2 versions', 'ie >= 10', 'iOS >= 7', 'Safari >= 7', 'Opera >= 25']
+    }))
+    .pipe($.rename('foundation-apps.css'))
+    .pipe(gulp.dest('./build/assets/css'))
+    .pipe($.if(production, $.minifyCss()))
+    .pipe($.rename('foundation-apps.min.css'))
+    .pipe(gulp.dest('./build/assets/css')));
+
+  // base package
+  merged.add(gulp.src('scss/base.scss')
+    .pipe($.sass({
+      outputStyle: 'nested',
+      errLogToConsole: true
+    }))
+    .pipe($.autoprefixer({
+      browsers: ['> 1%', 'last 2 versions', 'ie >= 10', 'iOS >= 7', 'Safari >= 7', 'Opera >= 25']
+    }))
+    .pipe($.rename('base-apps.css'))
+    .pipe(gulp.dest('./build/assets/css'))
+    .pipe($.if(production, $.minifyCss()))
+    .pipe($.rename('base-apps.min.css'))
+    .pipe(gulp.dest('./build/assets/css')));
+
+  merged.add(gulp.src('docs/assets/scss/app.scss')
     .pipe($.sass({
       includePaths: paths.sass.loadPaths,
       outputStyle: 'nested',
@@ -176,7 +232,10 @@ gulp.task('sass', function() {
     .pipe($.autoprefixer({
       browsers: ['> 1%', 'last 2 versions', 'ie >= 10', 'iOS >= 7', 'Safari >= 7', 'Opera >= 25']
     }))
-    .pipe(gulp.dest('./build/assets/css/'));
+    .pipe($.if(production, $.minifyCss()))
+    .pipe(gulp.dest('./build/assets/css/')));
+
+  return merged;
 });
 
 // Generate Sass settings file
@@ -201,20 +260,34 @@ gulp.task('sass:settings', function() {
 
 // Compile Foundation JavaScript
 gulp.task('javascript', function() {
+  var merged = merge();
+
+  // legacy package
+  merged.add(gulp.src(paths.javascript.foundation)
+    .pipe($.concat('foundation-apps.js'))
+    .pipe(gulp.dest('./build/assets/js'))
+    .pipe($.if(production, $.uglify()))
+    .pipe($.rename('foundation-apps.min.js'))
+    .pipe(gulp.dest('./build/assets/js')));
+
+  // base package
+  merged.add(gulp.src(paths.javascript.base)
+    .pipe($.concat('base-apps.js'))
+    .pipe(gulp.dest('./build/assets/js'))
+    .pipe($.if(production, $.uglify()))
+    .pipe($.rename('base-apps.min.js'))
+    .pipe(gulp.dest('./build/assets/js')));
+
   var dirs = paths.javascript.libs.concat(
-    paths.javascript.foundation,
-    paths.javascript.docs
-  );
-  return gulp.src(dirs)
-    .pipe($.if(production, $.uglify({
-      beautify: true,
-      mangle: false
-    }).on('error', function(e) {
-      console.log(e);
-    })))
+      paths.javascript.base,
+      paths.javascript.docs
+    );
+  merged.add(gulp.src(dirs)
+    .pipe($.if(production, $.uglify()))
     .pipe($.concat('app.js'))
-    .pipe(gulp.dest('./build/assets/js/'))
-  ;
+    .pipe(gulp.dest('./build/assets/js/')));
+
+  return merged;
 });
 
 // 7. SERVER
@@ -223,6 +296,17 @@ gulp.task('javascript', function() {
 gulp.task('server:start', function() {
   $.connect.server({
     root: './build',
+    middleware: function() {
+      return [
+        modRewrite(['^[^\\.]*$ /index.html [L]'])
+      ];
+    }
+  });
+});
+
+gulp.task('server:start:dist', function() {
+  $.connect.server({
+    root: './dist/docs',
     middleware: function() {
       return [
         modRewrite(['^[^\\.]*$ /index.html [L]'])
@@ -260,14 +344,17 @@ gulp.task('test', ['test:karma', 'test:sass'], function() {
 // Motion testing
 
 gulp.task('test:motion:compile', ['clean', 'sass', 'javascript'], function() {
-  gulp.src('./tests/motion/index.html')
-    .pipe(gulp.dest('./build'));
-  gulp.src('./tests/motion/templates/**/*.html')
+  var merged = merge();
+
+  merged.add(gulp.src('./tests/motion/index.html')
+    .pipe(gulp.dest('./build')));
+
+  merged.add(gulp.src('./tests/motion/templates/**/*.html')
     .pipe(routes({
       path: 'build/assets/js/routes.js',
       root: 'tests/motion'
     }))
-    .pipe(gulp.dest('./build/templates'));
+    .pipe(gulp.dest('./build/templates')));
 });
 
 gulp.task('test:motion', ['server:start', 'test:motion:compile'], function() {
@@ -277,81 +364,37 @@ gulp.task('test:motion', ['server:start', 'test:motion:compile'], function() {
 // 9. DISTRIBUTION BUILD
 // - - - - - - - - - - - - - - -
 
-// Distribution files
-gulp.task('production:enable', function(cb) { production = true; cb(); });
-
-gulp.task('build:dist', ['clean:dist', 'production:enable', 'build'], function() {
+gulp.task('copy:dist', function() {
   var merged = merge();
 
-  // legacy package
-  merged.add(gulp.src('scss/foundation.scss')
-    .pipe($.sass({
-      outputStyle: 'nested',
-      errLogToConsole: true
-    }))
-    .pipe($.autoprefixer({
-      browsers: ['> 1%', 'last 2 versions', 'ie >= 10', 'iOS >= 7', 'Safari >= 7', 'Opera >= 25']
-    }))
-    .pipe($.rename('foundation-apps.css'))
-    .pipe(gulp.dest('./dist/css'))
-    .pipe($.minifyCss())
-    .pipe($.rename('foundation-apps.min.css'))
+  // copy javascript
+  merged.add(gulp.src([
+      "./build/assets/js/foundation-apps*.js",
+      "./build/assets/js/base-apps*.js"
+    ])
+    .pipe(gulp.dest('./dist/js')));
+
+  // copy css
+  merged.add(gulp.src([
+      "./build/assets/css/foundation-apps*.css",
+      "./build/assets/css/base-apps*.css"
+    ])
     .pipe(gulp.dest('./dist/css')));
 
-  merged.add(gulp.src(paths.javascript.foundation)
-    .pipe($.concat('foundation-apps.js'))
-    .pipe(gulp.dest('./dist/js'))
-    .pipe($.uglify())
-    .pipe($.rename('foundation-apps.min.js'))
-    .pipe(gulp.dest('./dist/js')));
-
-  merged.add(gulp.src(paths.html.partials)
-    .pipe($.ngHtml2js({
-      prefix: 'components/',
-      moduleName: 'foundation',
-      declareModule: false
-    }))
-    .pipe($.concat('foundation-apps-templates.js'))
-    .pipe(gulp.dest('./dist/js'))
-    .pipe($.uglify())
-    .pipe($.rename('foundation-apps-templates.min.js'))
-    .pipe(gulp.dest('./dist/js')));
-
-  // base package
-  merged.add(gulp.src('scss/base.scss')
-    .pipe($.sass({
-      outputStyle: 'nested',
-      errLogToConsole: true
-    }))
-    .pipe($.autoprefixer({
-      browsers: ['> 1%', 'last 2 versions', 'ie >= 10', 'iOS >= 7', 'Safari >= 7', 'Opera >= 25']
-    }))
-    .pipe($.rename('base-apps.css'))
-    .pipe(gulp.dest('./dist/css'))
-    .pipe($.minifyCss())
-    .pipe($.rename('base-apps.min.css'))
-    .pipe(gulp.dest('./dist/css')));
-
-  merged.add(gulp.src(paths.javascript.base)
-    .pipe($.concat('base-apps.js'))
-    .pipe(gulp.dest('./dist/js'))
-    .pipe($.uglify())
-    .pipe($.rename('base-apps.min.js'))
-    .pipe(gulp.dest('./dist/js')));
-
-  merged.add(gulp.src(paths.html.partials)
-    .pipe($.ngHtml2js({
-      prefix: 'components/',
-      moduleName: 'base',
-      declareModule: false
-    }))
-    .pipe($.concat('base-apps-templates.js'))
-    .pipe(gulp.dest('./dist/js'))
-    .pipe($.uglify())
-    .pipe($.rename('base-apps-templates.min.js'))
-    .pipe(gulp.dest('./dist/js')));
-
-  merged.add(gulp.src("./build/**/*")
+  // copy docs
+  merged.add(gulp.src([
+    "./build/index.html",
+    "./build/partials/**/*",
+    "./build/templates/**/*",
+    "./build/assets/fonts/**/*",
+    "./build/assets/img/**/*",
+    "./build/assets/css/app.css",
+    "./build/assets/js/app.js",
+    "./build/assets/js/routes.js",
+    "./build/assets/js/templates.js"
+  ], {
+    base: "./build"
+  })
     .pipe(gulp.dest('./dist/docs')));
 
   return merged;
@@ -360,9 +403,17 @@ gulp.task('build:dist', ['clean:dist', 'production:enable', 'build'], function()
 // 10. NOW BRING IT TOGETHER
 // - - - - - - - - - - - - - - -
 
+gulp.task('production:enable', function(cb) { production = true; cb(); });
+
 // Build the documentation once
 gulp.task('build', function(cb) {
   runSequence('clean', ['copy', 'copy:partials', 'css', 'javascript', 'copy:templates'], function() {
+    cb();
+  });
+});
+
+gulp.task('build:dist', function(cb) {
+  runSequence('clean:dist', 'production:enable', 'build', 'copy:dist', function() {
     cb();
   });
 });
@@ -386,6 +437,25 @@ gulp.task('default', ['build', 'server:start'], function() {
   gulp.watch(paths.javascript.foundation.concat(paths.javascript.docs), ['javascript']);
 });
 
+// Build the documentation, start a test server, and re-compile when files change
+gulp.task('default:dist', ['build:dist', 'server:start:dist'], function() {
+
+  // Watch static files
+  gulp.watch(paths.html.base, ['copy', 'copy:dist']);
+
+  // Watch Angular templates
+  gulp.watch(paths.html.templates, ['copy:templates', 'copy:dist']);
+
+  // Watch Angular partials
+  gulp.watch(paths.html.partials, ['copy:partials', 'copy:dist']);
+
+  // Watch Sass
+  gulp.watch(['./docs/assets/scss/**/*', './scss/**/*'], ['css', 'copy:dist']);
+
+  // Watch JavaScript
+  gulp.watch(paths.javascript.foundation.concat(paths.javascript.docs), ['javascript', 'copy:dist']);
+});
+
 gulp.task('bump:patch', function() { return bump('patch'); });
 gulp.task('bump:minor', function() { return bump('minor'); });
 gulp.task('bump:major', function() { return bump('major'); });
@@ -404,7 +474,7 @@ function bump(importance) {
 }
 
 function publish() {
-  return gulp.src(['./package.json', './bower.json', './dist/**'])
+  return gulp.src(['./package.json', './bower.json', './dist/**/*'])
     // commit the changes
     .pipe($.git.commit('bump version'))
     // read only one file to get the version number
