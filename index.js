c76d1c401288ac0a6a82022fd8de545c161370b8
Brian Soumakian <bsoumakian@gmail.com>
2016-08-31 01:16:40 -0700

    add index.js for import support
    resolves https://github.com/base-apps/angular-base-apps/issues/74

---
+++ index.js
@@ -0,0 +1,10 @@
+require('fastclick');
+require('viewport-units-buggyfill');
+require('tether/tether');
+require('hammerjs/hammer');
+require('angular');
+require('angular-animate');
+require('angular-ui-router');
+require('./dist/js/base-apps');
+
+module.exports = 'angular-base-apps';
