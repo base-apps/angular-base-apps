714b094e701c296303a50f90156e6aab825dc1b1
Brian Soumakian <bsoumakian@gmail.com>
2016-08-31 14:21:52 -0700

    update templates

--- templates/angular-base-apps-template/js/app.js
+++ templates/angular-base-apps-template/js/app.js
@@ -1,1 +1,290 @@
-!function(){"use strict";var e="undefined"==typeof window?global:window;if("function"!=typeof e.require){var n={},r={},o={},t={}.hasOwnProperty,i=/^\.\.?(\/|$)/,u=function(e,n){for(var r,o=[],t=(i.test(n)?e+"/"+n:n).split("/"),u=0,a=t.length;u<a;u++)r=t[u],".."===r?o.pop():"."!==r&&""!==r&&o.push(r);return o.join("/")},a=function(e){return e.split("/").slice(0,-1).join("/")},s=function(n){return function(r){var o=u(a(n),r);return e.require(o,n)}},l=function(e,n){var o=null;o=h&&h.createHot(e);var t={id:e,exports:{},hot:o};return r[e]=t,n(t.exports,s(e),t),t.exports},c=function(e){return o[e]?c(o[e]):e},d=function(e,n){return c(u(a(e),n))},f=function(e,o){null==o&&(o="/");var i=c(e);if(t.call(r,i))return r[i].exports;if(t.call(n,i))return l(i,n[i]);throw new Error("Cannot find module '"+e+"' from '"+o+"'")};f.alias=function(e,n){o[n]=e};var m=/\.[^.\/]+$/,p=/\/index(\.[^\/]+)?$/,g=function(e){if(m.test(e)){var n=e.replace(m,"");t.call(o,n)&&o[n].replace(m,"")!==n+"/index"||(o[n]=e)}if(p.test(e)){var r=e.replace(p,"");t.call(o,r)||(o[r]=e)}};f.register=f.define=function(e,o){if("object"==typeof e)for(var i in e)t.call(e,i)&&f.register(i,e[i]);else n[e]=o,delete r[e],g(e)},f.list=function(){var e=[];for(var r in n)t.call(n,r)&&e.push(r);return e};var h=e._hmr&&new e._hmr(d,f,n,r);f._cache=r,f.hmr=h&&h.wrap,f.brunch=!0,e.require=f}}(),function(){var e;window;require.register("app.js",function(e,n,r){"use strict";function o(e){return e&&e.__esModule?e:{"default":e}}var t=n("fastclick"),i=o(t),u=n("angular"),a=o(u);n("angular-base-apps"),n("angular-icons/dist/open-iconic"),n("angular-icons/dist/ionicons"),n("angular-icons/dist/material-icons"),n("angular-dynamic-routing/dynamicRouting"),n("angular-dynamic-routing/dynamicRouting.animations"),n("./config/config-routes"),n("./modules");var s=function(e,n){e.otherwise("/"),n.html5Mode({enabled:!1,requireBase:!1})};s.$inject=["$urlRouterProvider","$locationProvider"];var l=function(){i["default"].FastClick.attach(document.body)};a["default"].module("application",["ui.router","ngAnimate","base","angularIcons.openIconic","angularIcons.ionicons","angularIcons.materialIcons","dynamicRouting","dynamicRouting.animations","application.home"]).config(s).run(l)}),require.register("config/config-routes.js",function(e,n,r){"use strict";angular.module("dynamicRouting").config(["$BaseAppsStateProvider",function(e){e.registerDynamicRoutes([{name:"home",url:"/",controller:"HomeController as home",path:"modules/home/home.html"}])}])}),require.register("modules/home/home-controller.js",function(e,n,r){"use strict";function o(e,n){if(!(e instanceof n))throw new TypeError("Cannot call a class as a function")}Object.defineProperty(e,"__esModule",{value:!0});var t=function(){function e(e,n){for(var r=0;r<n.length;r++){var o=n[r];o.enumerable=o.enumerable||!1,o.configurable=!0,"value"in o&&(o.writable=!0),Object.defineProperty(e,o.key,o)}}return function(n,r,o){return r&&e(n.prototype,r),o&&e(n,o),n}}(),i=function(){function e(){return o(this,e),this.messages=[],this}return t(e,[{key:"submitMessage",value:function(e){this.messages.push(e)}}]),e}();e["default"]=i}),require.register("modules/home/index.js",function(e,n,r){"use strict";function o(e){return e&&e.__esModule?e:{"default":e}}Object.defineProperty(e,"__esModule",{value:!0});var t=n("./home-controller"),i=o(t);e["default"]=angular.module("application.home",[]).controller("HomeController",i["default"]).name}),require.register("modules/index.js",function(e,n,r){"use strict";n("./home")}),require.alias("brunch/node_modules/deppack/node_modules/node-browser-modules/node_modules/path-browserify/index.js","path"),require.alias("brunch/node_modules/deppack/node_modules/node-browser-modules/node_modules/process/browser.js","process"),e=require("process"),require.register("___globals___",function(e,n,r){})}(),require("___globals___");
+(function() {
+  'use strict';
+
+  var globals = typeof window === 'undefined' ? global : window;
+  if (typeof globals.require === 'function') return;
+
+  var modules = {};
+  var cache = {};
+  var aliases = {};
+  var has = ({}).hasOwnProperty;
+
+  var expRe = /^\.\.?(\/|$)/;
+  var expand = function(root, name) {
+    var results = [], part;
+    var parts = (expRe.test(name) ? root + '/' + name : name).split('/');
+    for (var i = 0, length = parts.length; i < length; i++) {
+      part = parts[i];
+      if (part === '..') {
+        results.pop();
+      } else if (part !== '.' && part !== '') {
+        results.push(part);
+      }
+    }
+    return results.join('/');
+  };
+
+  var dirname = function(path) {
+    return path.split('/').slice(0, -1).join('/');
+  };
+
+  var localRequire = function(path) {
+    return function expanded(name) {
+      var absolute = expand(dirname(path), name);
+      return globals.require(absolute, path);
+    };
+  };
+
+  var initModule = function(name, definition) {
+    var hot = null;
+    hot = hmr && hmr.createHot(name);
+    var module = {id: name, exports: {}, hot: hot};
+    cache[name] = module;
+    definition(module.exports, localRequire(name), module);
+    return module.exports;
+  };
+
+  var expandAlias = function(name) {
+    return aliases[name] ? expandAlias(aliases[name]) : name;
+  };
+
+  var _resolve = function(name, dep) {
+    return expandAlias(expand(dirname(name), dep));
+  };
+
+  var require = function(name, loaderPath) {
+    if (loaderPath == null) loaderPath = '/';
+    var path = expandAlias(name);
+
+    if (has.call(cache, path)) return cache[path].exports;
+    if (has.call(modules, path)) return initModule(path, modules[path]);
+
+    throw new Error("Cannot find module '" + name + "' from '" + loaderPath + "'");
+  };
+
+  require.alias = function(from, to) {
+    aliases[to] = from;
+  };
+
+  var extRe = /\.[^.\/]+$/;
+  var indexRe = /\/index(\.[^\/]+)?$/;
+  var addExtensions = function(bundle) {
+    if (extRe.test(bundle)) {
+      var alias = bundle.replace(extRe, '');
+      if (!has.call(aliases, alias) || aliases[alias].replace(extRe, '') === alias + '/index') {
+        aliases[alias] = bundle;
+      }
+    }
+
+    if (indexRe.test(bundle)) {
+      var iAlias = bundle.replace(indexRe, '');
+      if (!has.call(aliases, iAlias)) {
+        aliases[iAlias] = bundle;
+      }
+    }
+  };
+
+  require.register = require.define = function(bundle, fn) {
+    if (typeof bundle === 'object') {
+      for (var key in bundle) {
+        if (has.call(bundle, key)) {
+          require.register(key, bundle[key]);
+        }
+      }
+    } else {
+      modules[bundle] = fn;
+      delete cache[bundle];
+      addExtensions(bundle);
+    }
+  };
+
+  require.list = function() {
+    var list = [];
+    for (var item in modules) {
+      if (has.call(modules, item)) {
+        list.push(item);
+      }
+    }
+    return list;
+  };
+
+  var hmr = globals._hmr && new globals._hmr(_resolve, require, modules, cache);
+  require._cache = cache;
+  require.hmr = hmr && hmr.wrap;
+  require.brunch = true;
+  globals.require = require;
+})();
+
+(function() {
+var global = window;
+var process;
+var __makeRelativeRequire = function(require, mappings, pref) {
+  var none = {};
+  var tryReq = function(name, pref) {
+    var val;
+    try {
+      val = require(pref + '/node_modules/' + name);
+      return val;
+    } catch (e) {
+      if (e.toString().indexOf('Cannot find module') === -1) {
+        throw e;
+      }
+
+      if (pref.indexOf('node_modules') !== -1) {
+        var s = pref.split('/');
+        var i = s.lastIndexOf('node_modules');
+        var newPref = s.slice(0, i).join('/');
+        return tryReq(name, newPref);
+      }
+    }
+    return none;
+  };
+  return function(name) {
+    if (name in mappings) name = mappings[name];
+    if (!name) return;
+    if (name[0] !== '.' && pref) {
+      var val = tryReq(name, pref);
+      if (val !== none) return val;
+    }
+    return require(name);
+  }
+};
+require.register("app.js", function(exports, require, module) {
+'use strict';
+
+var _fastclick = require('fastclick');
+
+var _fastclick2 = _interopRequireDefault(_fastclick);
+
+var _angular = require('angular');
+
+var _angular2 = _interopRequireDefault(_angular);
+
+require('angular-base-apps');
+
+require('angular-icons/dist/open-iconic');
+
+require('angular-icons/dist/ionicons');
+
+require('angular-icons/dist/material-icons');
+
+require('angular-dynamic-routing/dynamicRouting');
+
+require('angular-dynamic-routing/dynamicRouting.animations');
+
+require('./config/config-routes');
+
+require('./modules');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// Application Configuration
+
+
+// Route Configuration
+// Angular Base Apps Configuration
+var AppConfig = function AppConfig($urlProvider, $locationProvider) {
+  $urlProvider.otherwise('/');
+
+  $locationProvider.html5Mode({
+    enabled: false,
+    requireBase: false
+  });
+};
+
+// Module Configuration
+
+
+// Icon Configuration
+
+
+AppConfig.$inject = ['$urlRouterProvider', '$locationProvider'];
+
+var AppRun = function AppRun() {
+  _fastclick2.default.FastClick.attach(document.body);
+};
+
+_angular2.default.module('application', ['ui.router', 'ngAnimate',
+
+// base apps
+'base',
+
+// icons
+'angularIcons.openIconic', 'angularIcons.ionicons', 'angularIcons.materialIcons',
+
+// dynamic routing
+'dynamicRouting', 'dynamicRouting.animations',
+
+// modules
+'application.home']).config(AppConfig).run(AppRun);
+});
+
+require.register("config/config-routes.js", function(exports, require, module) {
+'use strict';
+
+angular.module('dynamicRouting').config(['$BaseAppsStateProvider', function (BaseAppsStateProvider) {
+  BaseAppsStateProvider.registerDynamicRoutes([{ "name": "home", "url": "/", "controller": "HomeController as home", "path": "modules/home/home.html" }]);
+}]);
+});
+
+require.register("modules/home/home-controller.js", function(exports, require, module) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var HomeController = function () {
+  function HomeController() {
+    _classCallCheck(this, HomeController);
+
+    this.messages = [];
+    return this;
+  }
+
+  _createClass(HomeController, [{
+    key: "submitMessage",
+    value: function submitMessage(message) {
+      this.messages.push(message);
+    }
+  }]);
+
+  return HomeController;
+}();
+
+exports.default = HomeController;
+});
+
+;require.register("modules/home/index.js", function(exports, require, module) {
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _homeController = require('./home-controller');
+
+var _homeController2 = _interopRequireDefault(_homeController);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = angular.module('application.home', []).controller('HomeController', _homeController2.default).name;
+});
+
+require.register("modules/index.js", function(exports, require, module) {
+'use strict';
+
+require('./home');
+});
+
+require.alias("brunch/node_modules/deppack/node_modules/node-browser-modules/node_modules/path-browserify/index.js", "path");
+require.alias("brunch/node_modules/deppack/node_modules/node-browser-modules/node_modules/process/browser.js", "process");process = require('process');require.register("___globals___", function(exports, require, module) {
+  
+});})();require('___globals___');
+
+
+//# sourceMappingURL=app.js.map
--- templates/angular-firebase-template/js/app.js
+++ templates/angular-firebase-template/js/app.js
@@ -1,1 +1,342 @@
-!function(){"use strict";var e="undefined"==typeof window?global:window;if("function"!=typeof e.require){var r={},n={},i={},o={}.hasOwnProperty,t=/^\.\.?(\/|$)/,a=function(e,r){for(var n,i=[],o=(t.test(r)?e+"/"+r:r).split("/"),a=0,s=o.length;a<s;a++)n=o[a],".."===n?i.pop():"."!==n&&""!==n&&i.push(n);return i.join("/")},s=function(e){return e.split("/").slice(0,-1).join("/")},u=function(r){return function(n){var i=a(s(r),n);return e.require(i,r)}},l=function(e,r){var i=null;i=h&&h.createHot(e);var o={id:e,exports:{},hot:i};return n[e]=o,r(o.exports,u(e),o),o.exports},c=function(e){return i[e]?c(i[e]):e},f=function(e,r){return c(a(s(e),r))},d=function(e,i){null==i&&(i="/");var t=c(e);if(o.call(n,t))return n[t].exports;if(o.call(r,t))return l(t,r[t]);throw new Error("Cannot find module '"+e+"' from '"+i+"'")};d.alias=function(e,r){i[r]=e};var m=/\.[^.\/]+$/,p=/\/index(\.[^\/]+)?$/,g=function(e){if(m.test(e)){var r=e.replace(m,"");o.call(i,r)&&i[r].replace(m,"")!==r+"/index"||(i[r]=e)}if(p.test(e)){var n=e.replace(p,"");o.call(i,n)||(i[n]=e)}};d.register=d.define=function(e,i){if("object"==typeof e)for(var t in e)o.call(e,t)&&d.register(t,e[t]);else r[e]=i,delete n[e],g(e)},d.list=function(){var e=[];for(var n in r)o.call(r,n)&&e.push(n);return e};var h=e._hmr&&new e._hmr(f,d,r,n);d._cache=n,d.hmr=h&&h.wrap,d.brunch=!0,e.require=d}}(),function(){var e;window;require.register("app.js",function(e,r,n){"use strict";function i(e){return e&&e.__esModule?e:{"default":e}}var o=r("fastclick"),t=i(o),a=r("angular"),s=i(a);r("angular-base-apps"),r("firebase"),r("angularfire");var u=r("./config/config-firebase"),l=i(u);r("angular-icons/dist/open-iconic"),r("angular-icons/dist/ionicons"),r("angular-icons/dist/material-icons"),r("angular-dynamic-routing/dynamicRouting"),r("angular-dynamic-routing/dynamicRouting.animations"),r("./config/config-routes"),r("./modules"),firebase.initializeApp(l["default"]);var c=function(e,r,n){e.otherwise("/"),r.html5Mode({enabled:!1,requireBase:!1}),n.registerUrl(l["default"].databaseURL)};c.$inject=["$urlRouterProvider","$locationProvider","$firebaseRefProvider"];var f=function(){t["default"].FastClick.attach(document.body)};s["default"].module("application",["ui.router","ngAnimate","firebase","base","angularIcons.openIconic","angularIcons.ionicons","angularIcons.materialIcons","dynamicRouting","dynamicRouting.animations","application.home"]).config(c).run(f)}),require.register("config/config-firebase.js",function(e,r,n){"use strict";n.exports={apiKey:"AIzaSyBkB1IvviOcPq4z8Rs7nijEdIa9n1IvRlU",authDomain:"angular-firebase-template.firebaseapp.com",databaseURL:"https://angular-firebase-template.firebaseio.com",storageBucket:""}}),require.register("config/config-routes.js",function(e,r,n){"use strict";angular.module("dynamicRouting").config(["$BaseAppsStateProvider",function(e){e.registerDynamicRoutes([{name:"home",url:"/",controller:"HomeController as home",path:"modules/home/home.html"}])}])}),require.register("modules/home/home-controller.js",function(e,r,n){"use strict";function i(e,r){if(!(e instanceof r))throw new TypeError("Cannot call a class as a function")}Object.defineProperty(e,"__esModule",{value:!0});var o=function(){function e(e,r){for(var n=0;n<r.length;n++){var i=r[n];i.enumerable=i.enumerable||!1,i.configurable=!0,"value"in i&&(i.writable=!0),Object.defineProperty(e,i.key,i)}}return function(r,n,i){return n&&e(r.prototype,n),i&&e(r,i),r}}(),t=function(){function e(r,n,o){i(this,e);var t,a=this;return t=r["default"].child("person"),this.person=n(t),t=r["default"].child("messages"),this.messages=o(t),this.messages.$resolved=!1,this.messages.$loaded().then(function(){a.messages.$resolved=!0}),a}return o(e,[{key:"submitMessage",value:function(e){this.messages.$add(e)}},{key:"updateName",value:function(e){this.person.name=e,this.person.$save()}}]),e}();e["default"]=t,t.$inject=["$firebaseRef","$firebaseObject","$firebaseArray"]}),require.register("modules/home/index.js",function(e,r,n){"use strict";function i(e){return e&&e.__esModule?e:{"default":e}}Object.defineProperty(e,"__esModule",{value:!0}),r("firebase");var o=r("./home-controller"),t=i(o);e["default"]=angular.module("application.home",["firebase"]).controller("HomeController",t["default"]).name}),require.register("modules/index.js",function(e,r,n){"use strict";r("./home")}),require.alias("brunch/node_modules/deppack/node_modules/node-browser-modules/node_modules/path-browserify/index.js","path"),require.alias("brunch/node_modules/deppack/node_modules/node-browser-modules/node_modules/process/browser.js","process"),e=require("process"),require.register("___globals___",function(e,r,n){})}(),require("___globals___");
+(function() {
+  'use strict';
+
+  var globals = typeof window === 'undefined' ? global : window;
+  if (typeof globals.require === 'function') return;
+
+  var modules = {};
+  var cache = {};
+  var aliases = {};
+  var has = ({}).hasOwnProperty;
+
+  var expRe = /^\.\.?(\/|$)/;
+  var expand = function(root, name) {
+    var results = [], part;
+    var parts = (expRe.test(name) ? root + '/' + name : name).split('/');
+    for (var i = 0, length = parts.length; i < length; i++) {
+      part = parts[i];
+      if (part === '..') {
+        results.pop();
+      } else if (part !== '.' && part !== '') {
+        results.push(part);
+      }
+    }
+    return results.join('/');
+  };
+
+  var dirname = function(path) {
+    return path.split('/').slice(0, -1).join('/');
+  };
+
+  var localRequire = function(path) {
+    return function expanded(name) {
+      var absolute = expand(dirname(path), name);
+      return globals.require(absolute, path);
+    };
+  };
+
+  var initModule = function(name, definition) {
+    var hot = null;
+    hot = hmr && hmr.createHot(name);
+    var module = {id: name, exports: {}, hot: hot};
+    cache[name] = module;
+    definition(module.exports, localRequire(name), module);
+    return module.exports;
+  };
+
+  var expandAlias = function(name) {
+    return aliases[name] ? expandAlias(aliases[name]) : name;
+  };
+
+  var _resolve = function(name, dep) {
+    return expandAlias(expand(dirname(name), dep));
+  };
+
+  var require = function(name, loaderPath) {
+    if (loaderPath == null) loaderPath = '/';
+    var path = expandAlias(name);
+
+    if (has.call(cache, path)) return cache[path].exports;
+    if (has.call(modules, path)) return initModule(path, modules[path]);
+
+    throw new Error("Cannot find module '" + name + "' from '" + loaderPath + "'");
+  };
+
+  require.alias = function(from, to) {
+    aliases[to] = from;
+  };
+
+  var extRe = /\.[^.\/]+$/;
+  var indexRe = /\/index(\.[^\/]+)?$/;
+  var addExtensions = function(bundle) {
+    if (extRe.test(bundle)) {
+      var alias = bundle.replace(extRe, '');
+      if (!has.call(aliases, alias) || aliases[alias].replace(extRe, '') === alias + '/index') {
+        aliases[alias] = bundle;
+      }
+    }
+
+    if (indexRe.test(bundle)) {
+      var iAlias = bundle.replace(indexRe, '');
+      if (!has.call(aliases, iAlias)) {
+        aliases[iAlias] = bundle;
+      }
+    }
+  };
+
+  require.register = require.define = function(bundle, fn) {
+    if (typeof bundle === 'object') {
+      for (var key in bundle) {
+        if (has.call(bundle, key)) {
+          require.register(key, bundle[key]);
+        }
+      }
+    } else {
+      modules[bundle] = fn;
+      delete cache[bundle];
+      addExtensions(bundle);
+    }
+  };
+
+  require.list = function() {
+    var list = [];
+    for (var item in modules) {
+      if (has.call(modules, item)) {
+        list.push(item);
+      }
+    }
+    return list;
+  };
+
+  var hmr = globals._hmr && new globals._hmr(_resolve, require, modules, cache);
+  require._cache = cache;
+  require.hmr = hmr && hmr.wrap;
+  require.brunch = true;
+  globals.require = require;
+})();
+
+(function() {
+var global = window;
+var process;
+var __makeRelativeRequire = function(require, mappings, pref) {
+  var none = {};
+  var tryReq = function(name, pref) {
+    var val;
+    try {
+      val = require(pref + '/node_modules/' + name);
+      return val;
+    } catch (e) {
+      if (e.toString().indexOf('Cannot find module') === -1) {
+        throw e;
+      }
+
+      if (pref.indexOf('node_modules') !== -1) {
+        var s = pref.split('/');
+        var i = s.lastIndexOf('node_modules');
+        var newPref = s.slice(0, i).join('/');
+        return tryReq(name, newPref);
+      }
+    }
+    return none;
+  };
+  return function(name) {
+    if (name in mappings) name = mappings[name];
+    if (!name) return;
+    if (name[0] !== '.' && pref) {
+      var val = tryReq(name, pref);
+      if (val !== none) return val;
+    }
+    return require(name);
+  }
+};
+require.register("app.js", function(exports, require, module) {
+'use strict';
+
+var _fastclick = require('fastclick');
+
+var _fastclick2 = _interopRequireDefault(_fastclick);
+
+var _angular = require('angular');
+
+var _angular2 = _interopRequireDefault(_angular);
+
+require('angular-base-apps');
+
+require('firebase');
+
+require('angularfire');
+
+var _configFirebase = require('./config/config-firebase');
+
+var _configFirebase2 = _interopRequireDefault(_configFirebase);
+
+require('angular-icons/dist/open-iconic');
+
+require('angular-icons/dist/ionicons');
+
+require('angular-icons/dist/material-icons');
+
+require('angular-dynamic-routing/dynamicRouting');
+
+require('angular-dynamic-routing/dynamicRouting.animations');
+
+require('./config/config-routes');
+
+require('./modules');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// Angular Base Apps Configuration
+firebase.initializeApp(_configFirebase2.default);
+
+// Icon Configuration
+
+
+// Firebase Configuration
+
+
+// Route Configuration
+
+
+// Module Configuration
+
+
+// Application Configuration
+var AppConfig = function AppConfig($urlProvider, $locationProvider, $firebaseRefProvider) {
+  $urlProvider.otherwise('/');
+
+  $locationProvider.html5Mode({
+    enabled: false,
+    requireBase: false
+  });
+
+  $firebaseRefProvider.registerUrl(_configFirebase2.default.databaseURL);
+};
+
+AppConfig.$inject = ['$urlRouterProvider', '$locationProvider', '$firebaseRefProvider'];
+
+var AppRun = function AppRun() {
+  _fastclick2.default.FastClick.attach(document.body);
+};
+
+_angular2.default.module('application', ['ui.router', 'ngAnimate',
+
+// firebase
+'firebase',
+
+// base apps
+'base',
+
+// icons
+'angularIcons.openIconic', 'angularIcons.ionicons', 'angularIcons.materialIcons',
+
+// dynamic routing
+'dynamicRouting', 'dynamicRouting.animations',
+
+// modules
+'application.home']).config(AppConfig).run(AppRun);
+});
+
+require.register("config/config-firebase.js", function(exports, require, module) {
+'use strict';
+
+module.exports = {
+  apiKey: 'AIzaSyBkB1IvviOcPq4z8Rs7nijEdIa9n1IvRlU',
+  authDomain: 'angular-firebase-template.firebaseapp.com',
+  databaseURL: 'https://angular-firebase-template.firebaseio.com',
+  storageBucket: ''
+};
+});
+
+require.register("config/config-routes.js", function(exports, require, module) {
+'use strict';
+
+angular.module('dynamicRouting').config(['$BaseAppsStateProvider', function (BaseAppsStateProvider) {
+  BaseAppsStateProvider.registerDynamicRoutes([{ "name": "home", "url": "/", "controller": "HomeController as home", "path": "modules/home/home.html" }]);
+}]);
+});
+
+require.register("modules/home/home-controller.js", function(exports, require, module) {
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var HomeController = function () {
+  function HomeController($firebaseRef, $firebaseObject, $firebaseArray) {
+    _classCallCheck(this, HomeController);
+
+    var vm = this,
+        ref;
+
+    ref = $firebaseRef.default.child('person');
+    this.person = $firebaseObject(ref);
+    ref = $firebaseRef.default.child('messages');
+    this.messages = $firebaseArray(ref);
+
+    this.messages.$resolved = false;
+    this.messages.$loaded().then(function () {
+      vm.messages.$resolved = true;
+    });
+
+    return vm;
+  }
+
+  _createClass(HomeController, [{
+    key: 'submitMessage',
+    value: function submitMessage(message) {
+      this.messages.$add(message);
+    }
+  }, {
+    key: 'updateName',
+    value: function updateName(name) {
+      this.person.name = name;
+      this.person.$save();
+    }
+  }]);
+
+  return HomeController;
+}();
+
+exports.default = HomeController;
+
+
+HomeController.$inject = ['$firebaseRef', '$firebaseObject', '$firebaseArray'];
+});
+
+require.register("modules/home/index.js", function(exports, require, module) {
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+require('firebase');
+
+var _homeController = require('./home-controller');
+
+var _homeController2 = _interopRequireDefault(_homeController);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = angular.module('application.home', ['firebase']).controller('HomeController', _homeController2.default).name;
+});
+
+require.register("modules/index.js", function(exports, require, module) {
+'use strict';
+
+require('./home');
+});
+
+require.alias("brunch/node_modules/deppack/node_modules/node-browser-modules/node_modules/path-browserify/index.js", "path");
+require.alias("brunch/node_modules/deppack/node_modules/node-browser-modules/node_modules/process/browser.js", "process");process = require('process');require.register("___globals___", function(exports, require, module) {
+  
+});})();require('___globals___');
+
+
+//# sourceMappingURL=app.js.map
